e271e00e030d87963c2a852e9c3b190de1e410ae
tom555cat <tongleiming1989@sina.com>
2016-03-21 18:02:07 +0800

    Initial Commit

---
+++ PersistenceLayer/PersistenceLayer.h
@@ -0,0 +1,13 @@
+//
+//  PersistenceLayer.h
+//  PersistenceLayer
+//
+//  Created by tom555cat on 16/3/21.
+//  Copyright © 2016年 H-T. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersistenceLayer : NSObject
+
+@end
---
+++ PersistenceLayer/PersistenceLayer.m
@@ -0,0 +1,13 @@
+//
+//  PersistenceLayer.m
+//  PersistenceLayer
+//
+//  Created by tom555cat on 16/3/21.
+//  Copyright © 2016年 H-T. All rights reserved.
+//
+
+#import "PersistenceLayer.h"
+
+@implementation PersistenceLayer
+
+@end
